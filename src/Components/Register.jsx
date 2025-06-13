1f1904dceac3fbeb1b74dee4671e33974014112c
Sandeep <160202390+Snxdxp4@users.noreply.github.com>
2025-06-13 11:15:18 +0530

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Api from "../api/axios";
-import { useNavigate } from "react-router";
 import { Link } from "react-router";
 
 function Register() {
@@ -11,7 +10,7 @@ function Register() {
   });
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+  const [register,setregister] = usestate(false);
   
   const register = async () => {
     setIsLoading(true);
@@ -19,7 +18,7 @@ function Register() {
       const response = await Api.post("/register", formData);
       if (response.status === 200) {
         console.log(response.data);
-        navigate("/auth/login")
+        setregister(true)
       }
     } catch (error) {
       setError(error.response?.data?.message || "An error occurred. Please try again later.");
@@ -83,7 +82,7 @@ function Register() {
             disabled={isLoading}
             className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 disabled:opacity-50"
           >
-            {isLoading ? "Registering..." : "Register"}
+           {register ? "Please Login now" : (isLoading ? "Registering..." : "Register")}
           </button>
         </form>
         <p className="text-center">
