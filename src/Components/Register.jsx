be9e6ef7fd9f0f84cbd60a66ca9d4757e7318cc5
Sandeep <160202390+Snxdxp4@users.noreply.github.com>
2025-06-13 11:16:06 +0530

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
   });
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [register,setregister] = usestate(false);
+  const [registed,setregisted] = usestate(false);
   
   const register = async () => {
     setIsLoading(true);
@@ -18,7 +18,7 @@ function Register() {
       const response = await Api.post("/register", formData);
       if (response.status === 200) {
         console.log(response.data);
-        setregister(true)
+        setregisted(true)
       }
     } catch (error) {
       setError(error.response?.data?.message || "An error occurred. Please try again later.");
@@ -82,7 +82,7 @@ function Register() {
             disabled={isLoading}
             className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 disabled:opacity-50"
           >
-           {register ? "Please Login now" : (isLoading ? "Registering..." : "Register")}
+           {registed ? "Please Login now" : (isLoading ? "Registering..." : "Register")}
           </button>
         </form>
         <p className="text-center">
